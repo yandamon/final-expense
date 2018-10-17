f8d22094c2122f629b23180133b9931d5c4968d9
abc <abc@example.com>
2018-10-17 14:47:42 +1100

    set up production build and server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname,'..','publick');
+const publicPath = path.join(__dirname,'..','public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -9,6 +10,6 @@ app.get('*',(req,res)=>{
     res.sendFile(path.join(publicPath,'index.html'));
 });
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is up!');
 })
